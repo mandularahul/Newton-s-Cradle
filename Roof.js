3364418c10354703fa50242cec604836b5da78e4
mandularahul <71830393+mandularahul@users.noreply.github.com>
2020-10-20 07:59:14 +0530

    Update Roof.js

--- Roof.js
+++ Roof.js
@@ -11,7 +11,7 @@ class Roof {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill(255);
+      fill("white");
       rect(pos.x, pos.y, this.width, this.height);
       
     }
